b42484f6b642b03e85605a95253afe144ef9f185
Manuel <s7435120@mail.zih.tu-dresden.de>
2013-06-02 15:49:18 +0200

    Update interface specification Add operations of appointment interface specification to the interface

--- sourcecode/ejbModule/st/cbse/umeat/appointment/AppointmentMgr.java
+++ sourcecode/ejbModule/st/cbse/umeat/appointment/AppointmentMgr.java
@@ -1,5 +1,23 @@
 package st.cbse.umeat.appointment;
 
+import java.util.List;
+
+import st.cbse.umeat.dto.AppointmentDetails;
+import st.cbse.umeat.dto.UserDetails;
+
 public class AppointmentMgr implements IAppointmentMgt {
 
+	@Override
+	public AppointmentDetails createAppointment(AppointmentDetails appDetails) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<AppointmentDetails> showAppointmentsOfDay(UserDetails user,
+			Long date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- sourcecode/ejbModule/st/cbse/umeat/appointment/IAppointmentMgt.java
+++ sourcecode/ejbModule/st/cbse/umeat/appointment/IAppointmentMgt.java
@@ -1,5 +1,14 @@
 package st.cbse.umeat.appointment;
 
+import java.util.List;
+
+import st.cbse.umeat.dto.AppointmentDetails;
+import st.cbse.umeat.dto.UserDetails;
+
 public interface IAppointmentMgt {
+	
+	public AppointmentDetails createAppointment(AppointmentDetails appDetails);
+
+	public List<AppointmentDetails> showAppointmentsOfDay(UserDetails user, Long date);
 
 }
