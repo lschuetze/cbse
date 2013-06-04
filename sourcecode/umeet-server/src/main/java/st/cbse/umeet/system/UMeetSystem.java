11fe86b9797e41f268f77f65d9f67a1f82013272
Manuel <s7435120@mail.zih.tu-dresden.de>
2013-06-04 20:02:06 +0200

    Update naming to UMeet

---
+++ sourcecode/umeet-server/src/main/java/st/cbse/umeet/system/UMeetSystem.java
@@ -0,0 +1,13 @@
+package st.cbse.umeet.system;
+
+import st.cbse.umeet.dto.AppointmentDetails;
+
+public class UMeetSystem implements IShowAppointmentOfTheDay {
+
+	@Override
+	public AppointmentDetails getAppointments() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
