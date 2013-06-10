c398d0471f165b153af59cb8343f260f9f3367a8
Ronny Marx <karlito1983@googlemail.com>
2013-06-10 22:23:00 +0200

    added a temporary debug message to the LoginController

--- sourcecode/umeet/umeet-web/src/main/java/st/cbse/umeet/controller/LoginController.java
+++ sourcecode/umeet/umeet-web/src/main/java/st/cbse/umeet/controller/LoginController.java
@@ -2,8 +2,10 @@ package st.cbse.umeet.controller;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+//import javax.enterprise.inject.Model;
 import javax.enterprise.inject.Produces;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -24,19 +26,26 @@ public class LoginController {
 	@Named
 	private LoginDetails loginDetails;
 	
+	private String email = null; 
+	
 	@PostConstruct
 	public void initNewLoginDetails(){
 		loginDetails = new LoginDetails();
 	}
 	
 	public void login(){
+		System.out.println(loginDetails.getEmail());
 		if(loginInterface.login(loginDetails.getEmail(), loginDetails.getPassword())){
-			//TODO @Ronny DoLogin!
+			this.email = loginDetails.getEmail();
 		} else {
 			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR,
 					"Username and password did not match"
 					, "Login Unsuccessful");
 			facesContext.addMessage(null, m);
 		}
+	}
+	
+	public String getEMail(){
+		return this.email;
 	}
 }
