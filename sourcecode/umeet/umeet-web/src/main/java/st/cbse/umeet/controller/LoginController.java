063d8315668d203aa2c12ab5bd106c364e8ba0ed
Ronny Marx <karlito1983@googlemail.com>
2013-06-10 15:27:12 +0200

    Added the modification the the LoginController which I forgot to add to the index...

--- sourcecode/umeet/umeet-web/src/main/java/st/cbse/umeet/controller/LoginController.java
+++ sourcecode/umeet/umeet-web/src/main/java/st/cbse/umeet/controller/LoginController.java
@@ -1,10 +1,14 @@
 package st.cbse.umeet.controller;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.enterprise.inject.Produces;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import st.cbse.umeet.dto.LoginDetails;
 import st.cbse.umeet.system.ILogin;
 
 @Model
@@ -16,8 +20,17 @@ public class LoginController {
 	@Inject
 	private ILogin loginInterface;
 	
-	public void login(String email, String password){
-		if(loginInterface.login(email, password)){
+	@Produces
+	@Named
+	private LoginDetails loginDetails;
+	
+	@PostConstruct
+	public void initNewLoginDetails(){
+		loginDetails = new LoginDetails();
+	}
+	
+	public void login(){
+		if(loginInterface.login(loginDetails.getEmail(), loginDetails.getPassword())){
 			//TODO @Ronny DoLogin!
 		} else {
 			FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR,
