ba684761710e4cad11b6e9ced6125bd2c11235a0
Lars Schuetze <Lars.Schuetze@tu-dresden.de>
2013-06-13 21:09:47 +0200

    fixed NPE when no one was invited

--- sourcecode/umeet/umeet-web/src/main/java/st/cbse/umeet/controller/AppointmentController.java
+++ sourcecode/umeet/umeet-web/src/main/java/st/cbse/umeet/controller/AppointmentController.java
@@ -35,19 +35,19 @@ public class AppointmentController implements Serializable {
 
 	@Inject
 	private ICreateAppointment appointmentCreator;
-	
+
 	@Inject
 	private IGetAllUsers userProvider;
 
 	private AppointmentDetails[] appointmentsOfTheDay;
-	
-	//private UserDetails[] invitableUsers;
+
+	// private UserDetails[] invitableUsers;
 
 	private Date startDate, endDate;
 	private String title, status, notes;
 	private boolean personal;
 	private Set<UserDetails> participants;
-	private List<AppointmentDetails> conflictingAppointments;	
+	private List<AppointmentDetails> conflictingAppointments;
 	private String chosenParticipant;
 	private String[] selectedInvitedUsers;
 
@@ -60,7 +60,6 @@ public class AppointmentController implements Serializable {
 		this.conflictingAppointments = conflictingAppointments;
 	}
 
-	
 	public String[] getSelectedInvitedUsers() {
 		return selectedInvitedUsers;
 	}
@@ -134,7 +133,7 @@ public class AppointmentController implements Serializable {
 	public void setParticipants(Set<UserDetails> participants) {
 		this.participants = participants;
 	}
-	
+
 	public List<UserDetails> getAllUsers() {
 		return userProvider.getAllUsers();
 	}
@@ -148,88 +147,85 @@ public class AppointmentController implements Serializable {
 
 		return result;
 	}
-	
 
 	public String createAppointment() {
-		
+
 		String email = (String) context.getExternalContext().getSessionMap()
 				.get("user");
 		List<String> partEmail = new LinkedList<String>();
-		
-		for(UserDetails detail:participants){
-			partEmail.add(detail.getEmail());
-		}
-		
 
-		if (appointmentCreator.createAppointment(email, startDate.getTime()
-				, endDate.getTime(), title, status, notes, personal, partEmail)) {		
-			return "showAppointments";
+		if (participants != null) {
+			for (UserDetails detail : participants) {
+				partEmail.add(detail.getEmail());
+			}
 		}
-		else {
-			System.out.println(email+ "\n" + startDate.getTime()+ "\n" + endDate.getTime()+ "\n" + title+ "\n" + status+ "\n" + notes+ "\n" + personal+ "\n" + partEmail);
-			
+
+		if (appointmentCreator.createAppointment(email, startDate.getTime(),
+				endDate.getTime(), title, status, notes, personal, partEmail)) {
+			return "showAppointments";
+		} else {
+			System.out.println(email + "\n" + startDate.getTime() + "\n"
+					+ endDate.getTime() + "\n" + title + "\n" + status + "\n"
+					+ notes + "\n" + personal + "\n" + partEmail);
+
 			try {
-				showConflicts(appointmentCreator.getConflicts(email, startDate.getTime()
-						, endDate.getTime(), status, personal
-						, partEmail));				
+				showConflicts(appointmentCreator.getConflicts(email,
+						startDate.getTime(), endDate.getTime(), status,
+						personal, partEmail));
 			} catch (Exception e) {
 				FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR,
 						"Stupid program monkey did something wrong here\n"
-						+ e.getMessage()
-						, "Appointment creation unsuccessful");
+								+ e.getMessage(),
+						"Appointment creation unsuccessful");
 				context.addMessage(null, m);
 			}
-			
-			
-			//return "login";
+
+			// return "login";
 			return "createAppointment";
 		}
-//		FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-//				"Username and password did not match"
-//				, "Create appointment unsuccessful");
-//		facesContext.addMessage(null, m);
-		//return "showAppointments";
+		// FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+		// "Username and password did not match"
+		// , "Create appointment unsuccessful");
+		// facesContext.addMessage(null, m);
+		// return "showAppointments";
 	}
-	
+
 	private void showConflicts(List<AppointmentDetails> conflicts) {
-		
+
 		String message = "There are conflicting appointments:\n";
-		
+
 		conflictingAppointments = conflicts;
-		
+
 		/*
-		for(AppointmentDetails details : conflicts){
-			message += "\t"; 
-			message += details.getCreator().getName() + " (\"";
-			message += details.getTitle() + "\" ";
-			message += "from " + new Date(details.getStartDate()).toString() + " ";
-			message += "to " + new Date(details.getEndDate()).toString() + " ";
-			message += ")";
-		}
-		*/
-		
-		FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-			message, "Appointment creation unsuccessful");
+		 * for(AppointmentDetails details : conflicts){ message += "\t"; message
+		 * += details.getCreator().getName() + " (\""; message +=
+		 * details.getTitle() + "\" "; message += "from " + new
+		 * Date(details.getStartDate()).toString() + " "; message += "to " + new
+		 * Date(details.getEndDate()).toString() + " "; message += ")"; }
+		 */
+
+		FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, message,
+				"Appointment creation unsuccessful");
 		context.addMessage(null, m);
 	}
 
-	public String invite(){
-		if(participants==null){
+	public String invite() {
+		if (participants == null) {
 			participants = new HashSet<UserDetails>();
 		}
-		for(UserDetails details : getAllUsers()){
-			if(details.getEmail().equals(chosenParticipant)){
+		for (UserDetails details : getAllUsers()) {
+			if (details.getEmail().equals(chosenParticipant)) {
 				participants.add(details);
 			}
 		}
 		return "createAppointment";
 	}
-	
-	public String disinvite(){
-		if(selectedInvitedUsers!=null){
-			for(String email:selectedInvitedUsers){
+
+	public String disinvite() {
+		if (selectedInvitedUsers != null) {
+			for (String email : selectedInvitedUsers) {
 				for (UserDetails details : getAllUsers()) {
-					if(details.getEmail().equals(email)){
+					if (details.getEmail().equals(email)) {
 						participants.remove(details);
 						break;
 					}
